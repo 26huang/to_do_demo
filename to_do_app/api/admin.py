6007d29687b3f8c2a4a457a29acca4d0a06e17dd
26huang <weishuoh@gmail.com>
2021-10-30 12:08:10 -0400

    added api CRUD functionality

--- to_do_app/api/admin.py
+++ to_do_app/api/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Task
 
 # Register your models here.
+
+@admin.register(Task)
+class ObjectTask(admin.ModelAdmin):
+    fields = ('title', 'completed')
+
+    list_display = ('id', 'title', 'completed')
+    list_editable = ('completed',)
